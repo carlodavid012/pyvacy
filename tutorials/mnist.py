c97d31f04752af895d59a5c802e7e4650f0c9abe
CarloDavid <carlo12david@gmail.com>
2020-03-04 15:38:47 +0800

    fixed dataloader

--- tutorials/mnist.py
+++ tutorials/mnist.py
@@ -127,7 +127,7 @@ if __name__ == '__main__':
         ])
     )
 
-    X, y = next(iter(DataLoader(test_dataset, batch_size=len(test_dataset))))
+    X, y = next(iter(torch.utils.data.DataLoader(test_dataset, batch_size=len(test_dataset))))
     X, y  = X.to('cuda'), y.to('cuda')
 
     y_pred = classifier(X).max(1)[1]
